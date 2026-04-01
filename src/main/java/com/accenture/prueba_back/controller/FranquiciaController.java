bb4700a653f9c03bd1c01cd5ea7e1c9e00a1b9e3
Andres <andresdavidmazo@gmail.com>
2026-04-01 14:34:18 -0500

    Actualizando controller con servicio para agregar franquicias

--- src/main/java/com/accenture/prueba_back/controller/FranquiciaController.java
+++ src/main/java/com/accenture/prueba_back/controller/FranquiciaController.java
@@ -2,6 +2,7 @@ package com.accenture.prueba_back.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accenture.prueba_back.dto.FranquiciaDto;
+import com.accenture.prueba_back.service.IFranquiciaService;
 
 @RestController
 @RequestMapping("/v1")
 public class FranquiciaController {
 	private static final Logger log = LoggerFactory.getLogger(FranquiciaController.class);
+	
+	@Autowired
+	private IFranquiciaService franquiciaService;
 	
 	@CrossOrigin
 	@GetMapping(path = "/")
@@ -26,6 +31,7 @@ public class FranquiciaController {
 	@PostMapping(path = "/nueva-franquicia")
 	public Boolean agregarFranquicia(@RequestBody FranquiciaDto request) {
 		log.info("Ingresando a FranquiciaController metodo agregarFranquicia");
+		franquiciaService.addFranquicia(request.getNombre());
 		return true;
 	}
 }
